c3d30d87bc20f1a1b242d58e67d27a9c63f5a1ee
fengzai6 <lfq0120@gmail.com>
2025-07-06 19:26:34 +0800

    feat: 在项目中新增 joi 进行环境变量数据验证

--- src/config/config.module.ts
+++ src/config/config.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { defaultConfig } from './config.default';
+import { validationSchema } from './env.validation';
 import { developmentConfig } from './env/config.development';
 import { productionConfig } from './env/config.production';
 
@@ -8,6 +9,7 @@ import { productionConfig } from './env/config.production';
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
+      validationSchema,
       load: [
         defaultConfig,
         process.env.NODE_ENV === 'production'
---
+++ src/config/env.validation.ts
@@ -0,0 +1,41 @@
+import * as Joi from 'joi';
+
+export const validationSchema = Joi.object({
+  // Database
+  DATABASE_URL: Joi.string().uri({ scheme: ['postgres'] }),
+  DATABASE_HOST: Joi.string(),
+  DATABASE_PORT: Joi.number().port().default(5432),
+  DATABASE_USERNAME: Joi.string(),
+  DATABASE_PASSWORD: Joi.string(),
+  DATABASE_NAME: Joi.string(),
+  DATABASE_SYNCHRONIZE: Joi.boolean().default(false),
+
+  // Initial Admin User
+  DEFAULT_ADMIN_USERNAME: Joi.string().required(),
+  DEFAULT_ADMIN_PASSWORD: Joi.string().required(),
+
+  // JWT
+  JWT_SECRET: Joi.string().required(),
+
+  // Server
+  PORT: Joi.number().port(),
+
+  // Swagger
+  SWAGGER_OPEN: Joi.boolean().default(true),
+
+  // Snowflake ID Generator
+  WORKER_ID: Joi.number().integer().min(0).max(31).default(0),
+  DATACENTER_ID: Joi.number().integer().min(0).max(31).default(0),
+
+  // Node Environment
+  NODE_ENV: Joi.string()
+    .valid('development', 'production', 'test')
+    .default('development'),
+})
+  .xor('DATABASE_URL', 'DATABASE_HOST') // 使用 URL 或单独的连接参数
+  .with('DATABASE_HOST', [
+    'DATABASE_PORT',
+    'DATABASE_USERNAME',
+    'DATABASE_PASSWORD',
+    'DATABASE_NAME',
+  ]);
