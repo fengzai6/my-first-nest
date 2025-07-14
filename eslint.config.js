62d8a53ff3586e716cc6a898eeb4d0a1ada9f9d2
fengzai6 <lfq0120@gmail.com>
2025-07-14 23:45:12 +0800

    chore: 移除旧的 ESLint 配置，添加新的 ESLint 和 Commitlint 配置，更新 package.json 以支持 lint-staged 和 husky

---
+++ eslint.config.js
@@ -0,0 +1,38 @@
+import tsPlugin from '@typescript-eslint/eslint-plugin';
+import tsParser from '@typescript-eslint/parser';
+import prettierConfig from 'eslint-config-prettier';
+import prettierPlugin from 'eslint-plugin-prettier';
+import globals from 'globals';
+
+export default [
+  {
+    ignores: ['.eslintrc.js', 'dist/', 'node_modules/'],
+  },
+  {
+    files: ['**/*.ts', '**/*.js'],
+    languageOptions: {
+      parser: tsParser,
+      parserOptions: {
+        project: 'tsconfig.json',
+        tsconfigRootDir: import.meta.dirname,
+        sourceType: 'module',
+      },
+      globals: {
+        ...globals.node,
+        ...globals.jest,
+      },
+    },
+    plugins: {
+      '@typescript-eslint': tsPlugin,
+      prettier: prettierPlugin,
+    },
+    rules: {
+      ...tsPlugin.configs.recommended.rules,
+      ...prettierConfig.rules,
+      '@typescript-eslint/interface-name-prefix': 'off',
+      '@typescript-eslint/explicit-function-return-type': 'off',
+      '@typescript-eslint/explicit-module-boundary-types': 'off',
+      '@typescript-eslint/no-explicit-any': 'off',
+    },
+  },
+];
