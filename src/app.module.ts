e9b577eeefa6a1a7377acfa372b563289d1b5958
fengzai6 <lfq0120@gmail.com>
2025-07-14 23:45:30 +0800

    chore: 移除 AppModule 中的中间件配置，简化代码结构

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,4 @@
-import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
+import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { appGuards } from './common/guards/app-guards';
@@ -12,14 +12,4 @@ import { StaticModule } from './shared/static/static.module';
   controllers: [AppController],
   providers: [AppService, ...appGuards],
 })
-export class AppModule implements NestModule {
-  configure(consumer: MiddlewareConsumer) {
-    // consumer
-    //   // 应用日志中间件
-    //   .apply(logger)
-    //   // 排除不需要记录日志的路径
-    //   .exclude('/swagger', '/favicon.ico')
-    //   // 使用对象形式配置路由，采用新版路径格式
-    //   .forRoutes('/*path');
-  }
-}
+export class AppModule {}
--- src/common/use/use.note.md
+++ src/common/use/use.note.md
@@ -112,9 +112,12 @@
 export class AppModule implements NestModule {
   configure(consumer: MiddlewareConsumer) {
     consumer
-      .apply(LoggerMiddleware)
-      .exclude({ path: 'health', method: RequestMethod.GET })
-      .forRoutes({ path: '*', method: RequestMethod.ALL });
+      // 应用日志中间件
+      .apply(logger)
+      // 排除不需要记录日志的路径
+      .exclude('/swagger', '/favicon.ico')
+      // 使用对象形式配置路由，采用新版路径格式
+      .forRoutes('/*path');
   }
 }
 ```
