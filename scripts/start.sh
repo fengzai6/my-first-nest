398ca31473891c9a99ceb45433efb9350ea44bf8
Nacho.L <112751823+fengzai6@users.noreply.github.com>
2025-07-17 08:35:12 +0800

    feat: docker draft

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,12 +1,21 @@
 #!/bin/sh
 
-export NODE_ENV=production
+export NODE_ENV=${NODE_ENV:-production}
 
 # 设置当脚本执行过程中，如果出现错误，则立即退出
 set -e
 
-# 初始化数据库
-ts-node database/manage.ts init
-
-# 启动服务
-node dist/main
+# 在生产环境中，运行编译后的 JS 文件
+if [ "$NODE_ENV" = "production" ]; then
+  echo "Running in production mode..."
+  # 初始化数据库
+  node dist/database/manage.js init
+  # 启动服务
+  node dist/src/main
+else
+  echo "Running in development mode..."
+  # 在开发环境中，使用 ts-node
+  ts-node -r tsconfig-paths/register database/manage.ts init
+  # 启动服务
+  nest start
+fi
