4326cd2f4b395013a66ba1d86f0ed45886bf0c67
Nacho.L <112751823+fengzai6@users.noreply.github.com>
2025-07-28 14:08:46 +0800

    feat: sidebar

--- client/src/components/app-sidebar/index.tsx
+++ client/src/components/app-sidebar/index.tsx
@@ -1,53 +1,84 @@
+import NestJsIcon from "@/assets/nestjs.svg";
+import { HomeOutlined, UserOutlined } from "@ant-design/icons";
+import { useLocation } from "react-router";
 import {
   Sidebar,
   SidebarContent,
+  SidebarFooter,
   SidebarGroup,
   SidebarGroupContent,
   SidebarGroupLabel,
+  SidebarHeader,
   SidebarMenu,
   SidebarMenuButton,
   SidebarMenuItem,
 } from "../ui/sidebar";
-import { UserOutlined, HomeOutlined } from "@ant-design/icons";
-import { Link, useLocation } from "react-router";
+import { NavUser } from "./nav-user";
+import { useUserStore } from "@/stores/user";
 
-const menuItems = [
+const sidebarGroups = [
   {
-    label: "首页",
-    icon: <HomeOutlined />,
-    path: "/home",
+    label: "For U",
+    content: [
+      {
+        name: "Home",
+        icon: <HomeOutlined />,
+        path: "/home",
+      },
+    ],
   },
   {
-    label: "用户管理",
-    icon: <UserOutlined />,
-    path: "/users",
+    label: "后台管理",
+    content: [
+      {
+        name: "用户管理",
+        icon: <UserOutlined />,
+        path: "/users",
+      },
+    ],
   },
 ];
 
 export const AppSidebar = () => {
   const location = useLocation();
+  const user = useUserStore((state) => state.user);
 
   return (
     <Sidebar collapsible="icon">
+      <SidebarHeader>
+        <div className="flex items-center space-x-2 px-2">
+          <img src={NestJsIcon} alt="Logo" className="h-5 w-5" />
+          <div>My First Nest</div>
+        </div>
+      </SidebarHeader>
       <SidebarContent>
-        <SidebarGroup>
-          <SidebarGroupLabel>管理系统</SidebarGroupLabel>
-          <SidebarGroupContent>
-            <SidebarMenu>
-              {menuItems.map(({ label, icon, path }) => (
-                <SidebarMenuItem key={label}>
-                  <SidebarMenuButton asChild isActive={location.pathname.startsWith(path)}>
-                    <Link to={path}>
-                      {icon}
-                      <span>{label}</span>
-                    </Link>
-                  </SidebarMenuButton>
-                </SidebarMenuItem>
-              ))}
-            </SidebarMenu>
-          </SidebarGroupContent>
-        </SidebarGroup>
+        {sidebarGroups.map((item, index) => {
+          return (
+            <SidebarGroup key={index}>
+              <SidebarGroupLabel>{item.label}</SidebarGroupLabel>
+              <SidebarGroupContent>
+                <SidebarMenu>
+                  {item.content.map((contentItem, i) => (
+                    <SidebarMenuItem key={contentItem.name + i}>
+                      <SidebarMenuButton
+                        isActive={location.pathname.startsWith(
+                          contentItem.path,
+                        )}
+                      >
+                        {contentItem.icon}
+                        <span>{contentItem.name}</span>
+                      </SidebarMenuButton>
+                    </SidebarMenuItem>
+                  ))}
+                </SidebarMenu>
+              </SidebarGroupContent>
+            </SidebarGroup>
+          );
+        })}
       </SidebarContent>
+      <SidebarFooter>
+        <NavUser user={user} />
+      </SidebarFooter>
     </Sidebar>
   );
 };
