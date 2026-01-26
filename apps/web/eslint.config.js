3200b3ea794c3a1d5b99e2505f37f5174cc9001e
Nacho.L <112751823+fengzai6@users.noreply.github.com>
2026-01-26 10:31:37 +0800

    refactor: use yarn@4

--- apps/web/eslint.config.js
+++ apps/web/eslint.config.js
@@ -4,7 +4,7 @@ import reactRefresh from "eslint-plugin-react-refresh";
 import globals from "globals";
 import tseslint from "typescript-eslint";
 
-export default tseslint.config([
+export default tseslint.defineConfig([
   {
     ignores: ["dist/**", "node_modules/**"],
   },
